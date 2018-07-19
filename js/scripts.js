bb87497262a1f4f2c1a12febe330bb0540948502
praxyfarhana <praxyfarhana@gmail.com>
2018-07-19 12:37:55 +0300

    created variables

--- js/scripts.js
+++ js/scripts.js
@@ -7,5 +7,10 @@ function Account (initial,withdrawal,deposit){
 $(document).ready(function() {
   $("form#account").submit(function(event) {
     event.preventDefault();
+    var InputtedInitial = $("input#init").val();
+    var InputtedWithdrawal = $("input#with").val();
+    var InputtedDeposit = $("input#dep".val();
+    var newAccount = new Account(InputtedInitial, InputtedWithdrawal, InputtedDeposit);
+    
   })
 })
